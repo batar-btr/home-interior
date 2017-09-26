421595a6527e58606e0ac58a881ac83da2e6e8e1
batar-btr <btrn86@gmail.com>
2017-09-27 02:26:37 +0300

    add main section

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,14 @@
-console.log('hello!');
+(function (){
+    'use strict';
+
+    function showNavBar () {
+        var navBar = document.querySelector('.navbar');
+        navBar.classList.toggle('active');
+    }
+
+    var burger = document.querySelector('.burger');
+
+    burger.addEventListener('click', showNavBar);
+
+})();
+
