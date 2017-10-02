6d53ad58536455a2835cdd2614ccf7ca1c52339d
batar-btr <btrn86@gmail.com>
2017-10-02 22:51:28 +0300

    add fullpage js

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,24 @@
 (function (){
     'use strict';
 
-    function showNavBar () {
-        var navBar = document.querySelector('.navbar');
-        navBar.classList.toggle('active');
-    }
+    // function showNavBar () {
+    //     var navBar = document.querySelector('.navbar');
+    //     navBar.classList.toggle('active');
+    // }
 
-    var burger = document.querySelector('.burger');
+    // var burger = document.querySelector('.burger');
 
-    burger.addEventListener('click', showNavBar);
+    // burger.addEventListener('click', showNavBar);
+    // setTimeout(function() {
+    //     window.scrollTo(0, 100);
+    // } , 100);
 
 })();
 
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+        scrollOverflow: true,
+        anchors:['firstPage', 'secondPage', 'thirdPage'],
+        menu: '#myMenu'
+    });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -30,9 +30,10 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 
 gulp.task('scripts', function() {
     return gulp.src([ // Берем все необходимые библиотеки
-        'app/libs/angular/angular.min.js',
         'app/libs/jquery/dist/jquery.min.js',
-        'app/libs/bootstrap-sass/assets/javascripts/bootstrap.min.js'
+        'app/libs/fullpage.js/vendors/scrolloverflow.min.js',
+        'app/libs/fullpage.js/jquery.fullPage.min.js'
+        
         ])
         .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
         .pipe(uglify()) // Сжимаем JS файл
